2f9595d786ddd19e676e5101f636c661d211d182
amsohag007 <amsohag007@gmail.com>
2020-10-12 23:19:52 +0600

    context api pracitise

---
+++ src/components/Booklist.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { BookContext } from '../context/BookContext'
+import Bookdetails from './Bookdetails'
+
+function Booklist() {
+    const {books} = useContext(BookContext);
+    return books.length?(
+        <div className="book-list">
+        <ul>
+            {books.map(book => {
+                return (<Bookdetails book={book} key={book.id}/>)
+            })}
+        </ul>
+        </div>
+    ):(<div className = "empty">No books to read ;)</div>)
+}
+
+export default Booklist
